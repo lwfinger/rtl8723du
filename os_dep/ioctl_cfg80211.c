4b25c819fad580eeff62c3df5696b11f0e5a5060
Larry Finger <Larry.Finger@lwfinger.net>
2023-05-15 09:39:38 -0500

    rtl8723du: Fix build on kernel 6.3
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -173,6 +173,8 @@ u8 rtw_cfg80211_ch_switch_notify(struct adapter *adapter, u8 ch, u8 bw, u8 offse
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 19, 2)
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef);
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0))
+	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0, 0);
 #else
 	cfg80211_ch_switch_notify(adapter->pnetdev, &chdef, 0);
 #endif
