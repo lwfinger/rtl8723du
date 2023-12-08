ae03b0861351f72808405ddda80e8aab105bcfce
Larry Finger <Larry.Finger@lwfinger.net>
2023-12-08 16:30:50 -0600

    rtl8723du: Fix build errors with kernel 6.7
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -3794,10 +3794,17 @@ exit:
 }
 
 static int cfg80211_rtw_change_beacon(struct wiphy *wiphy, struct net_device *ndev,
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
+		struct cfg80211_ap_update *params)
+#else
 		struct cfg80211_beacon_data *info)
+#endif
 {
 	int ret = 0;
 	struct adapter *adapter = (struct adapter *)rtw_netdev_priv(ndev);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
+	struct cfg80211_beacon_data *info = &params->beacon;
+#endif
 
 	RTW_INFO(FUNC_NDEV_FMT"\n", FUNC_NDEV_ARG(ndev));
 
